999a5c340f5f29cedec3fb4f27d27badbd3a4dd9
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2020-01-29 15:14:44 -0400

    Add Project

---
+++ commands.md
@@ -0,0 +1,5 @@
+composer require annotations
+php -S 127.0.0.1:8000 -t public\
+ 
+ 
+php bin/console debug:router
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+*/
+class BlogController extends  AbstractController {
+
+    private const POSTS = [
+        ['id' => 1, 'slug' => 'hello-world', 'title' => 'Hello World'],
+        ['id' => 2, 'slug' => 'another-post', 'title' => 'Another Post'],
+        ['id' => 3, 'slug' => 'last-example', 'title' => 'This is the last example']
+    ];
+
+
+    /**
+     * @Route("/{page}", name="blog_list", defaults={"page": 5})
+     * @param $page
+     * @return JsonResponse
+     */
+    public function list($page = 1){
+        return new JsonResponse(
+            [
+                'page' => $page,
+                'data' => array_map(function ($item){
+                    return $this->generateUrl('blog_by_slug', ['slug' => $item['slug']]);
+                }, self::POSTS)
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="blog_by_id")
+     * @param $id
+     * @return JsonResponse
+     */
+    public function post($id){
+        return new JsonResponse(
+            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+        );
+    }
+
+    /**
+     * @Route("/{slug}", name="blog_by_slug")
+     * @param $slug
+     * @return JsonResponse
+     */
+    public function postBySlug($slug){
+        return new JsonResponse(self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]);
+    }
+}
